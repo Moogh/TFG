ebf6cfb6688e134b6bbc6068ad09d2c361b56d3d
Moogh <salomon.grc@gmail.com>
2025-09-01 00:59:24 -0500

    Se crean los estilos

---
+++ src/components/Card.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+    return (
+        <div>
+            <h1>this is a card</h1>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Sidebar } from './components/sidebar'
+import { Sidebar } from './components/Sidebar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
